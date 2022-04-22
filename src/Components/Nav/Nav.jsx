46fd3a17de4d2594156ee42f66cdacfe2f6e330a
Shivam Dureja <shivamdureja13@gmail.com>
2022-04-22 19:21:42 +0530

    Nav Component updated

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './nav.css'
+import {BiHome} from 'react-icons/bi'
+import {BiUserCircle} from 'react-icons/bi'
+import {FiBookOpen} from 'react-icons/fi'
+import {RiServiceLine} from 'react-icons/ri'
+import {AiFillMessage} from 'react-icons/ai'
+import { useState } from 'react'
+
 const Nav = () => {
+  const [activeNav,setActiveNav] = useState('#');
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" onClick={()=>setActiveNav('#')}className={activeNav === '#' ? 'active' : ''}><BiHome/></a>
+      <a href="#about" onClick={()=>setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''} ><BiUserCircle/></a>
+      <a href="#exp" onClick={()=>setActiveNav('#exp')} className={activeNav === '#exp' ? 'active' : ''}><FiBookOpen/></a>
+      <a href="#services" onClick={()=>setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine/></a>
+      <a href="#contact" onClick={()=>setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><AiFillMessage/></a>
+    </nav>
   )
 }
 
-export default Nav
+export default Nav;
