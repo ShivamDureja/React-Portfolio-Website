188be50481579948e2abbac4c899bc1fbc74fd32
Shivam Dureja <shivamdureja13@gmail.com>
2022-04-25 21:15:25 +0530

    src edited

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from './Components/Header/Header'
 import Nav from './Components/Nav/Nav'
 import About from './Components/About/About'
 import Experience from './Components/Experience/Experience'
-import Services from './Components/Services/Services'
 import Portfolio from './Components/Portfolio/Portfolio'
 import Contact from './Components/Contact/Contact'
 import Footer from './Components/Footer/Footer'
@@ -14,7 +13,6 @@ function App() {
       <Nav />
       <About />
       <Experience />
-      <Services/>
       <Portfolio/>
       <Contact/>
       <Footer/>
